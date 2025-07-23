172fd4cb6193a938a533dc4c9fa65b2c80d9b120
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-07-23 14:50:46 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import LoginPage from './pages/LoginPage'
-import SalesPage from './pages/SalesPageWrapper'
+import SalesPageWrapper from './pages/SalesPageWrapper'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
