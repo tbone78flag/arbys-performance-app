ec352c0b9c11c4de7dfac5a3fce30e2136d70eb7
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-07-23 16:04:03 -0600

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen bg-red-100 flex flex-col items-center p-4">
+    <div className="w-screen min-h-screen bg-red-100 flex flex-col items-center justify-start p-4">
       {children}
     </div>
   )
