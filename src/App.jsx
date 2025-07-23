4e5bd706550b53f526e4fee992800f560aae6fe8
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-07-22 20:39:17 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from './supabaseClient'
-import { Routes, Route, Link } from 'react-router-dom'
-import SalesPage from './pages/SalesPage.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
