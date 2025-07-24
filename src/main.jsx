5163574201aa161eb491032efc6173f87c177fe0
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-07-24 13:08:57 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<LoginPage />} />
         <Route path="/app" element={<App />} />
         <Route path="/sales" element={<SalesPageWrapper />} />
+        <Route path="/food" element={<FoodPageWrapper />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
