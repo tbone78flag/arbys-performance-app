358e6d0182497cbaa26bd6241e8253c026b44d32
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-08-10 13:52:06 -0600

    Changes for phone layout

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -68,8 +68,8 @@ export default function LoginPage() {
 
         <button
           type="submit"
-          className="bg-red-600 text-white px-4 py-2 rounded w-full hover:bg-red-700 text-white"
-        >
+          className="bg-red-600 text-white px-4 py-2 rounded w-full hover:bg-red-700"
+>
           Log In
         </button>
       </form>
