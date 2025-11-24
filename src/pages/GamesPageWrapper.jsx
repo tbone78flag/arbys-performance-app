9f7c994ea4e8a23c129a3b8e5b680290721be7bb
Tristan P. <tristan.tbone.pacheco@gmail.com>
2025-11-23 17:15:53 -0700

    Update GamesPageWrapper.jsx

--- src/pages/GamesPageWrapper.jsx
+++ src/pages/GamesPageWrapper.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../supabaseClient'
-import ManagerPage from './GamesPage'
+import GamesPage from './GamesPage'
 import Layout from '../components/Layout'
 
 export default function GamesPageWrapper() {
