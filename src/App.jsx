c785c9ab4119c27c341446d6eaf017be31e56758
Tristan Pacheco <tristan.tbone.pacheco@gmail.com>
2025-12-11 19:29:19 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
       className="bg-white text-red-600 border border-red-600 px-4 py-2 rounded hover:bg-red-50 mt-4"
       onClick={() => navigate('/experience')}
     >
-      Go to Guest Experience Page
+      Go to Experience Page
     </button>
 
     {profile?.role === 'manager' && (
