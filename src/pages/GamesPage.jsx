1acdfe21eadcb3af2c9b38cd4f43735f8ce94dc8
Tristan Pacheco <tristan.tbone.pacheco@gmail.com>
2025-12-11 20:30:58 -0700

    Update GamesPage.jsx

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -56,9 +56,15 @@ export default function GamesPage({ profile }) {
 
       {/* Accessible to all team members */}
       <div className="mb-4">
-        <h2 className="text-lg font-semibold">Total Points</h2>
-        <p>Team members can redeem points here:</p>
-        {/* Later: form or button */}
+        <h2 className="text-lg font-semibold">Points Page</h2>
+        <p>Click here to see or redeem your points:</p>
+        {/* Go to points page */}
+        <button
+        className="bg-white text-red-600 border border-red-600 px-4 py-2 rounded hover:bg-red-50 mt-4"
+        onClick={() => navigate('/points')}
+      >
+        Go to Points Page
+      </button>
       </div>
 
       {/* Accordion for mini games */}
